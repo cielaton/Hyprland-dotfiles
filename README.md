c5a8d7ba798e0e096c2314d6fdb9aae57db0f2b5
kartorias1 <vuwin24680@gmail.com>
2023-03-07 21:13:13 +0700

    yet another rebase in today

--- README.md
+++ README.md
@@ -19,6 +19,10 @@
   > Note that this is the Wayland compatible version of Rofi.
 - Fetch : [`Nitch`](https://github.com/ssleert/nitch)
 
+## Miscellaneous:
+
+- Icon fonts: `backup/`
+
 ## Nedded packages:
 
 (all of the above components) plus [`hyprland`](https://github.com/hyprwm/Hyprland) `swaybg` `swayidle` `swaylock` `wlroots` `wl-clipboard` `mako` `grim` `slurp` `wf-recorder` `light` `yad` `mpv` `viewnior` `imagemagick` `xfce-polkit` `xorg-xwayland`
--- sync_local_config_to_repo.sh
+++ sync_local_config_to_repo.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
 
 rsync -a --delete-after "$HOME"/.config/hypr/ ./hypr
-rsync -a --delete-after "$HOME"/.config/kitty ./kitty
-rsync -a --delete-after "$HOME"/.config/starship ./starship
+rsync -a --delete-after "$HOME"/.config/kitty/ ./kitty
+rsync -a --delete-after "$HOME"/.config/starship/ ./starship
 rsync -a --delete-after "$HOME"/.config/waybar/ ./waybar
 cp "$HOME"/.zshrc .
-rsync -a --delete-after "$HOME"/backup ./backup
+rsync -a --delete-after "$HOME"/backup/ ./backup
