7c3ab5286d82e17ad9e8d58d7961e652fe7bac00
kartorias1 <vuwin24680@gmail.com>
2023-08-11 20:57:45 +0700

    added obs support for xwayland app like Discord, changed wallpaper, minor refactor in waybar

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,10 @@ end
 set -g fish_greeting
 
 starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/kiet/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
