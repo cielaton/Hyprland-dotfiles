8933ebab49a7cc3d8941dc005db9017b21c8a2c2
kartorias1 <vuwin24680@gmail.com>
2023-08-14 12:30:10 +0700

    changed wallpaper again and added more choices

--- fish/functions/stable-diffusion-ui-server.fish
+++
@@ -1,4 +0,0 @@
-function stable-diffusion-ui-server --description 'alias stable-diffusion-ui-server=env HSA_OVERRIDE_GFX_VERSION=10.3.0 stable-diffusion-ui-server'
-  env HSA_OVERRIDE_GFX_VERSION=10.3.0 stable-diffusion-ui-server $argv
-        
-end
